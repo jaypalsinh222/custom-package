5865305915882b1a424ad9d564c46703876f4bff
User <jaypalsinhk.cirklestudio@gmail.com>
2023-08-25 15:46:01 +0530

    cuatom-package

---
+++ src/ContactServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cirkle\Contact;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
+}
